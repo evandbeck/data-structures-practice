22273d32bdcccf53570b82cc355c59ae69041b80
Evan Beck <evandrydenbeck@gmail.com>
2022-08-16 12:00:19 -0400

    push, pop, shift methods

--- data-structures/DoublyLinkedLists.js
+++ data-structures/DoublyLinkedLists.js
@@ -14,4 +14,47 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+    // Adding a node to the end of the Doubly Linked List
+    push(val) {
+        let newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    // Removing a node from the end of the Doubly Linked List
+    pop() {
+        if(!this.head) return undefined;
+        let poppedNode = this.tail;
+        if(this.length = 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return current;
+    }
+    // Removing a node from the beginning of the Doubly Linked List
+    shift() {
+        if(this.length === 0) return undefined;
+        let oldHead = this.head;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        }
+        this.head = oldHead.next;
+        this.head.prev = null;
+        oldHead.next = null;
+        this.length--;
+        return oldHead;
+    }
 }
