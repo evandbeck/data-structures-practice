89f1915c6cd3b0a29e0a4d1fe3861b56d99b67f2
Evan Beck <evandrydenbeck@gmail.com>
2022-08-18 11:00:27 -0400

    Stack class

---
+++ data-structures/Stacks + Queues.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    // Add a value to the top of the stack
+    push() {
+
+    }
+    // Remove a value from the top of the stack
+    pop() {
+        
+    }
+}
