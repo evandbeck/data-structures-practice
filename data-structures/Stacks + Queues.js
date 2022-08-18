80fa676ffd468df5e407ac533c309ddc0827d220
Evan Beck <evandrydenbeck@gmail.com>
2022-08-18 11:07:11 -0400

    push and pop methods

--- data-structures/Stacks + Queues.js
+++ data-structures/Stacks + Queues.js
@@ -11,11 +11,27 @@ class Stack {
         this.size = 0;
     }
     // Add a value to the top of the stack
-    push() {
-
+    push(val) {
+        let newNode = new Node(val);
+        if(!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
     }
     // Remove a value from the top of the stack
     pop() {
-        
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.val;
     }
 }
