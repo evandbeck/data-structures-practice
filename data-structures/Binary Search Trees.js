c5332ee2c4fc66f1facbf20a24d51665404ac427
Evan Beck <evandrydenbeck@gmail.com>
2022-08-19 23:24:42 -0400

    insert method

--- data-structures/Binary Search Trees.js
+++ data-structures/Binary Search Trees.js
@@ -9,4 +9,36 @@ class BinarySearchTrees {
     constructor() {
         this.root = null;
     }
+    // Insert
+    insert(value) {
+        let newNode = new Node(value);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while(true) {
+                if(value === current.value) return undefined;
+                if(value < current.value) {
+                    if(current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if(value > current.value) {
+                    if(current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+    // Find
+    find() {
+
+    }
 }
