523ba5433b54c6fb98257e988b070c7623f3c145
Evan Beck <evandrydenbeck@gmail.com>
2022-08-19 23:45:01 -0400

    find method

--- data-structures/Binary Search Trees.js
+++ data-structures/Binary Search Trees.js
@@ -38,7 +38,20 @@ class BinarySearchTrees {
         }
     }
     // Find
-    find() {
-
+    find(value) {
+        if(this.root === null) return false;
+        let current = this.root;
+        let found = false;
+        while(current && !found) {
+            if(value < current.value) {
+                current = current.left;
+            } else if(value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
     }
 }
