50a73d87faa4fdab80269b265b2f95e742f90c97
Evan Beck <evandrydenbeck@gmail.com>
2022-08-23 17:59:40 -0400

    Depth First Search methods

--- data-structures/Binary Search Trees.js
+++ data-structures/Binary Search Trees.js
@@ -68,4 +68,37 @@ class BinarySearchTrees {
         }
         return data;
     }
+    // Depth First PreOrder Search
+    DFSPreOrder() {
+        let data = [];
+        function traverse(node) {
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+    // Depth First PostOrder Search
+    DFSPostOrder() {
+        let data = [];
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value)
+        }
+        traverse(this.root);
+        return data;
+    }
+    // Depth First InOrder Search
+    DFSInOrder() {
+        let data = [];
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            data.push(node.value)
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
 }
