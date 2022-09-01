a2a3bed92ffd3cf06f56f691c87fdc03541747aa
Evan Beck <evandrydenbeck@gmail.com>
2022-09-01 14:20:44 -0400

    function refactor

--- data-structures/Hash Tables.js
+++ data-structures/Hash Tables.js
@@ -1,9 +1,11 @@
 // Hash (Strings Only)
 function hash(key, arrayLen) {
     let total = 0;
-    for (let char of key) {
-        let value = char.charCodeAt(o) - 96
-        total = (total + value) % arrayLen;
+    let prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i]
+        let value = char.charCodeAt(0) - 96
+        total = (total * prime + value) % arrayLen;
     }
     return total;
 }
