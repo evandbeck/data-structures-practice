8590f6eb7df339deef44731f04789c89737aa955
Evan Beck <evandrydenbeck@gmail.com>
2022-09-01 16:02:28 -0400

    Set method

--- data-structures/Hash Tables.js
+++ data-structures/Hash Tables.js
@@ -9,3 +9,29 @@ function hash(key, arrayLen) {
     }
     return total;
 }
+
+class HashTable {
+    constructor(size=53) {
+        this.keyMap = new Array(size);
+    }
+    _hash(key) {
+        let total = 0;
+        let prime = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i]
+            let value = char.charCodeAt(0) - 96
+            total = (total * prime + value) % this.keyMap.length;
+        }
+        return total;
+    }
+    // Separate Chaining
+    // SET
+    set(key,value) {
+        let index = this._hash(key);
+        if(!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+        return index;
+    }
+}
