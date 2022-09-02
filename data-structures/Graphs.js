c89598cbb8bbc2e98af6f65a63208b5e6079b5e4
Evan Beck <evandrydenbeck@gmail.com>
2022-09-02 11:35:16 -0400

    Graph addEdge method

--- data-structures/Graphs.js
+++ data-structures/Graphs.js
@@ -8,4 +8,17 @@ class Graph {
     addVertext(vertex) {
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+    // Adding an Edge
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+    // Removing an Edge
+    removeEdge() {
+
+    }
+    // Removing a Vertex
+    removeVertex() {
+        
+    }
 }
