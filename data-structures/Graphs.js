35008d7077441244fbaac7384152fce8e92d21be
Evan Beck <evandrydenbeck@gmail.com>
2022-09-02 19:59:07 -0400

    DFS Recursive method

--- data-structures/Graphs.js
+++ data-structures/Graphs.js
@@ -30,4 +30,21 @@ class Graph {
         }
         delete this.adjacencyList[vertex];
     }
+    // DFS Recursive
+    depthFirstRecursive(start) {
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex) {
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    return dfs(neighbor);
+                }
+            });
+        }) (start);
+    }
 }
