bc93cd65107325ddac6fbecc7ca60bc7940b3b9a
Evan Beck <evandrydenbeck@gmail.com>
2022-09-02 11:39:01 -0400

    Graph removeEdge method

--- data-structures/Graphs.js
+++ data-structures/Graphs.js
@@ -14,11 +14,16 @@ class Graph {
         this.adjacencyList[vertex2].push(vertex1);
     }
     // Removing an Edge
-    removeEdge() {
-
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            v => v !== vertex2
+        )
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            v => v !== vertex1
+        )
     }
     // Removing a Vertex
     removeVertex() {
-        
+
     }
 }
