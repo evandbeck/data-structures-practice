d378b4ca6783fb3b56f5da314278058c37e6553a
Evan Beck <evandrydenbeck@gmail.com>
2022-09-02 20:46:41 -0400

    DFS Iterative method

--- data-structures/Graphs.js
+++ data-structures/Graphs.js
@@ -47,4 +47,25 @@ class Graph {
             });
         }) (start);
     }
+    // DFS Iterative
+    depthFirstIterative(start) {
+        const result = [];
+        const visited = {};
+        const stack = [start];
+        let currentVertex;
+
+        visited[start] = true;
+        while(stack.length) {
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentIndex].forEach(neighbor => {
+                if(!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            });
+            return result;
+        }
+    }
 }
